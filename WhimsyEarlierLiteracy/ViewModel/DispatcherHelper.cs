43202984bf5629c4fc4418d44796913ac1f9b06c
Jerel R Hass <agies1@hotmail.com>
2013-01-23 11:34:28 -0800

    removing previous solution

--- WhimsyEarlierLiteracy/ViewModel/DispatcherHelper.cs
+++
@@ -1,9 +0,0 @@
-using Windows.UI.Core;
-
-namespace WhimsyEarlierLiteracy.ViewModel
-{
-    public static class DispatcherHelper
-    {
-        public static CoreDispatcher Dispatcher { get; set; }
-    }
-}
