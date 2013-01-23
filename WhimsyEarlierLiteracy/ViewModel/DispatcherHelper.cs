2d6fc2ce3dfe5c0f9b9c00195a33c43a681fcd00
Jerel R Hass <agies1@hotmail.com>
2013-01-23 11:22:59 -0800

    Minor file changes

---
+++ WhimsyEarlierLiteracy/ViewModel/DispatcherHelper.cs
@@ -0,0 +1,9 @@
+using Windows.UI.Core;
+
+namespace WhimsyEarlierLiteracy.ViewModel
+{
+    public static class DispatcherHelper
+    {
+        public static CoreDispatcher Dispatcher { get; set; }
+    }
+}
